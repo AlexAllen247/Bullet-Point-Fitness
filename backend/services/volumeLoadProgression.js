66e9de24d82a137e07ca99eb57e54a98e90a93c6
Alex Allen <99975765+AlexAllen247@users.noreply.github.com>
2024-01-18 13:10:03 +0100

    added volume loadprogression to backend

---
+++ backend/services/volumeLoadProgression.js
@@ -0,0 +1,35 @@
+function findNewWorkoutOptions(
+  currentReps,
+  currentWeight,
+  increasePercentage = 2,
+) {
+  const MIN_REPS = 5;
+  const MAX_REPS = 20;
+  const WEIGHT_INCREMENT = 2.5;
+  const MAX_WEIGHT = 1000;
+
+  let currentVolumeLoad = currentReps * currentWeight;
+  let targetVolumeLoad = currentVolumeLoad * (1 + increasePercentage / 100);
+
+  let possibleCombinations = [];
+  let minRepsForWeight = MAX_REPS + 1;
+
+  for (
+    let weight = WEIGHT_INCREMENT;
+    weight <= MAX_WEIGHT;
+    weight += WEIGHT_INCREMENT
+  ) {
+    for (let reps = MIN_REPS; reps < minRepsForWeight; reps++) {
+      let newVolumeLoad = reps * weight;
+      if (newVolumeLoad >= targetVolumeLoad) {
+        possibleCombinations.push({ reps, weight, newVolumeLoad });
+        minRepsForWeight = reps;
+        break;
+      }
+    }
+  }
+
+  return possibleCombinations;
+}
+
+console.log(findNewWorkoutOptions(10, 20));

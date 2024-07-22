b4a86b2c27b09a2b1c797e05b1041323aeaae31e
Alex Allen <99975765+AlexAllen247@users.noreply.github.com>
2024-07-22 13:06:10 +0200

    the progress charts for each workout are looking great, need a review of all functionality and then onto design

--- frontend/src/components/ProgressGraph.js
+++ frontend/src/components/ProgressGraph.js
@@ -10,6 +10,7 @@ import {
   Tooltip,
   Legend,
 } from "chart.js";
+import ChartDataLabels from "chartjs-plugin-datalabels";
 import workoutService from "../services/workout";
 
 ChartJS.register(
@@ -20,17 +21,29 @@ ChartJS.register(
   CategoryScale,
   Tooltip,
   Legend,
+  ChartDataLabels,
 );
 
+const COLORS = [
+  "rgba(75,192,192,1)",
+  "rgba(192,75,75,1)",
+  "rgba(75,192,75,1)",
+  "rgba(192,192,75,1)",
+  "rgba(75,75,192,1)",
+  "rgba(192,75,192,1)",
+  "rgba(75,75,75,1)",
+  "rgba(192,192,192,1)",
+];
+
 const ProgressGraph = ({ userId }) => {
-  const [chartData, setChartData] = useState(null);
+  const [chartDataList, setChartDataList] = useState([]);
 
   useEffect(() => {
     const fetchWorkouts = async () => {
       try {
         const workouts = await workoutService.get(userId);
         const data = processWorkoutData(workouts);
-        setChartData(data);
+        setChartDataList(data);
       } catch (error) {
         console.error("Error fetching workout data", error);
       }
@@ -40,91 +53,110 @@ const ProgressGraph = ({ userId }) => {
   }, [userId]);
 
   const processWorkoutData = (workouts) => {
-    const exercisesMap = new Map();
+    return workouts.map((workout, workoutIndex) => {
+      const exercisesMap = new Map();
 
-    workouts.forEach((workout) => {
       workout.exercises.forEach((exercise) => {
         const { title } = exercise.exerciseId;
         if (!exercisesMap.has(title)) {
           exercisesMap.set(title, { weights: [], reps: [], dates: [] });
         }
         exercise.performance.forEach((performance) => {
-          exercisesMap.get(title).weights.push(performance.weight);
-          exercisesMap.get(title).reps.push(performance.reps);
-          exercisesMap
-            .get(title)
-            .dates.push(new Date(performance.date).toLocaleDateString());
+          const weight = performance.weight;
+          const reps = performance.reps;
+          const date = new Date(performance.date).toLocaleDateString();
+          if (weight != null && reps != null) {
+            exercisesMap.get(title).weights.push(weight);
+            exercisesMap.get(title).reps.push(reps);
+            exercisesMap.get(title).dates.push(date);
+          }
         });
       });
+
+      const combinedDatasets = Array.from(exercisesMap.entries()).map(
+        ([title, data], index) => ({
+          label: title,
+          data: data.dates.map((date, index) => ({
+            x: date,
+            y: data.weights[index],
+            reps: data.reps[index],
+          })),
+          fill: false,
+          borderColor: COLORS[index % COLORS.length], // Assign a color from the array
+          tension: 0.1,
+          backgroundColor: COLORS[index % COLORS.length], // Assign a color from the array
+          borderWidth: 1,
+        }),
+      );
+
+      return {
+        workoutIndex,
+        labels:
+          exercisesMap.size > 0 ? exercisesMap.values().next().value.dates : [],
+        datasets: combinedDatasets,
+        title: `Workout ${workoutIndex + 1}`,
+      };
     });
-
-    const weightsDatasets = Array.from(exercisesMap.entries()).map(
-      ([title, data]) => ({
-        label: `${title} (Weight)`,
-        data: data.weights,
-        yAxisID: "y1",
-        fill: false,
-        borderColor: "rgba(75,192,192,1)",
-        tension: 0.1,
-      }),
-    );
-
-    const repsDatasets = Array.from(exercisesMap.entries()).map(
-      ([title, data]) => ({
-        label: `${title} (Reps)`,
-        data: data.reps,
-        yAxisID: "y2",
-        fill: false,
-        borderColor: "rgba(192,75,75,1)",
-        tension: 0.1,
-      }),
-    );
-
-    return {
-      labels:
-        exercisesMap.size > 0 ? exercisesMap.values().next().value.dates : [],
-      datasets: [...weightsDatasets, ...repsDatasets],
-    };
   };
 
   return (
     <div>
-      <h2>Progress Graph</h2>
-      {chartData ? (
-        <Line
-          data={chartData}
-          options={{
-            scales: {
-              y1: {
-                type: "linear",
-                position: "left",
-                beginAtZero: true,
-                title: {
-                  display: true,
-                  text: "Weight (kg)",
+      <h2>Progress Graphs</h2>
+      {chartDataList.length > 0 ? (
+        chartDataList.map((chartData, index) => (
+          <div key={index}>
+            <h3>{chartData.title}</h3>
+            <Line
+              data={{
+                labels: chartData.labels,
+                datasets: chartData.datasets,
+              }}
+              options={{
+                scales: {
+                  y: {
+                    type: "linear",
+                    position: "left",
+                    beginAtZero: true,
+                    title: {
+                      display: true,
+                      text: "Weight (kg)",
+                    },
+                  },
+                  x: {
+                    title: {
+                      display: true,
+                      text: "Date",
+                    },
+                  },
                 },
-              },
-              y2: {
-                type: "linear",
-                position: "right",
-                beginAtZero: true,
-                title: {
-                  display: true,
-                  text: "Reps",
+                plugins: {
+                  datalabels: {
+                    align: "end",
+                    anchor: "end",
+                    formatter: (value, context) => {
+                      const { reps } = value;
+                      return `Reps: ${reps}`;
+                    },
+                    color: "rgba(192,75,75,1)",
+                    font: {
+                      weight: "bold",
+                    },
+                  },
+                  tooltip: {
+                    callbacks: {
+                      label: function (context) {
+                        const label = context.dataset.label || "";
+                        const weight = context.parsed.y;
+                        const reps = context.raw.reps;
+                        return `${label}: Weight: ${weight}kg, Reps: ${reps}`;
+                      },
+                    },
+                  },
                 },
-                grid: {
-                  drawOnChartArea: false,
-                },
-              },
-              x: {
-                title: {
-                  display: true,
-                  text: "Date",
-                },
-              },
-            },
-          }}
-        />
+              }}
+            />
+          </div>
+        ))
       ) : (
         <p>Loading data...</p>
       )}

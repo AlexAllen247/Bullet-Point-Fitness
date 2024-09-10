18baa6200467e33e8f03e3b7925340d21ee21566
Alex Allen <99975765+AlexAllen247@users.noreply.github.com>
2024-09-10 11:03:45 +0200

    adjusting the client info form, once done that site can be launched and begin testing workout tracker

--- frontend/src/components/ClientInfoForm.jsx
+++ frontend/src/components/ClientInfoForm.jsx
@@ -4,6 +4,7 @@ import clientInfoFormService from "../services/clientInfoForm";
 import { useNavigate } from "react-router-dom";
 
 const ClientInfoForm = ({ notify }) => {
+  const [isFirstTimeUser, setIsFirstTimeUser] = useState(null);
   const [sessionsPerWeek, setSessionsPerWeek] = useState("");
   const [fitnessLevel, setFitnessLevel] = useState("");
   const [goals, setGoals] = useState("");
@@ -72,72 +73,174 @@ const ClientInfoForm = ({ notify }) => {
         <div>
           <h1 style={styles.header}>Program Form</h1>
         </div>
-        <Card className="my-3" style={styles.card} border="danger">
-          <Card.Body>
-            <Form onSubmit={handleSubmit} style={styles.form}>
-              <Form.Group className="mb-3">
-                <Form.Label htmlFor="fitnessLevel" style={styles.label}>
-                  Fitness Level
-                </Form.Label>
-                <Form.Control
-                  as="select"
-                  value={fitnessLevel}
-                  onChange={(e) => setFitnessLevel(e.target.value)}
-                >
-                  <option value="">Select Your Stage</option>
-                  <option value="Stage 1 (Beginner)">Stage 1 (Beginner)</option>
-                  <option value="Stage 2 (Intermediate)">
-                    Stage 2 (Intermediate)
-                  </option>
-                  <option value="Stage 3 (Advanced)">Stage 3 (Advanced)</option>
-                </Form.Control>
-              </Form.Group>
-              <Form.Group className="mb-3">
-                <Form.Label htmlFor="goals" style={styles.label}>
-                  Goals
-                </Form.Label>
-                <Form.Control
-                  as="select"
-                  value={goals}
-                  onChange={(e) => setGoals(e.target.value)}
-                >
-                  <option value="">Select Fitness Goal</option>
-                  <option value="fat-loss">Fat Loss</option>
-                  <option value="build-muscle">Build Muscle</option>
-                  <option value="build-strength">Build Strength</option>
-                  <option value="all-of-the-above">All of the Above</option>
-                </Form.Control>
-              </Form.Group>
-              <Form.Group className="mb-3">
-                <Form.Label style={styles.label}>Sessions Per Week</Form.Label>
-                <Form.Control
-                  as="select"
-                  value={sessionsPerWeek}
-                  onChange={(e) => setSessionsPerWeek(e.target.value)}
-                >
-                  <option value="">Select sessions per week</option>
-                  <option value="3">3 Sessions</option>
-                  <option value="4">4 Sessions</option>
-                </Form.Control>
-              </Form.Group>
-              <Form.Group className="mb-3">
-                <Form.Label htmlFor="injuriesOrConditions" style={styles.label}>
-                  Any Injuries or Underlying Health Conditions
-                </Form.Label>
-                <Form.Control
-                  as="textarea"
-                  rows={3}
-                  value={injuriesOrConditions}
-                  onChange={(e) => setInjuriesOrConditions(e.target.value)}
-                  placeholder="Write here..."
-                />
-              </Form.Group>
-              <Button type="submit" variant="danger" disabled={loading}>
-                {loading ? "Submitting..." : "Submit"}
-              </Button>
-            </Form>
-          </Card.Body>
-        </Card>
+
+        {isFirstTimeUser === null && (
+          <Card className="my-3" style={styles.card} border="danger">
+            <Card.Body>
+              <Form style={styles.form}>
+                <Form.Group className="mb-3">
+                  <Form.Label style={styles.label}>
+                    Are you a first-time user or returning user?
+                  </Form.Label>
+                  <div>
+                    <Button
+                      variant="primary"
+                      onClick={() => setIsFirstTimeUser(true)}
+                    >
+                      First-time User
+                    </Button>
+                    <Button
+                      variant="secondary"
+                      onClick={() => setIsFirstTimeUser(false)}
+                      style={{ marginLeft: "10px" }}
+                    >
+                      Returning User
+                    </Button>
+                  </div>
+                </Form.Group>
+              </Form>
+            </Card.Body>
+          </Card>
+        )}
+        {isFirstTimeUser === true && (
+          <Card className="my-3" style={styles.card} border="danger">
+            <Card.Body>
+              <Form onSubmit={handleSubmit} style={styles.form}>
+                <Form.Group className="mb-3">
+                  <Form.Label htmlFor="fitnessLevel" style={styles.label}>
+                    Fitness Level
+                  </Form.Label>
+                  <Form.Control
+                    as="select"
+                    value={fitnessLevel}
+                    onChange={(e) => setFitnessLevel(e.target.value)}
+                  >
+                    <option value="">Select Your Stage</option>
+                    <option value="Stage 1 (Beginner)">
+                      Stage 1 (Beginner)
+                    </option>
+                    <option value="Stage 2 (Intermediate)">
+                      Stage 2 (Intermediate)
+                    </option>
+                    <option value="Stage 3 (Advanced)">
+                      Stage 3 (Advanced)
+                    </option>
+                  </Form.Control>
+                </Form.Group>
+                <Form.Group className="mb-3">
+                  <Form.Label htmlFor="goals" style={styles.label}>
+                    Goals
+                  </Form.Label>
+                  <Form.Control
+                    as="select"
+                    value={goals}
+                    onChange={(e) => setGoals(e.target.value)}
+                  >
+                    <option value="">Select Fitness Goal</option>
+                    <option value="fat-loss">Fat Loss</option>
+                    <option value="build-muscle">Build Muscle</option>
+                    <option value="build-strength">Build Strength</option>
+                    <option value="all-of-the-above">All of the Above</option>
+                  </Form.Control>
+                </Form.Group>
+                <Form.Group className="mb-3">
+                  <Form.Label style={styles.label}>
+                    Sessions Per Week
+                  </Form.Label>
+                  <Form.Control
+                    as="select"
+                    value={sessionsPerWeek}
+                    onChange={(e) => setSessionsPerWeek(e.target.value)}
+                  >
+                    <option value="">Select sessions per week</option>
+                    <option value="3">3 Sessions</option>
+                    <option value="4">4 Sessions</option>
+                  </Form.Control>
+                </Form.Group>
+                <Form.Group className="mb-3">
+                  <Form.Label
+                    htmlFor="injuriesOrConditions"
+                    style={styles.label}
+                  >
+                    Any Injuries or Underlying Health Conditions
+                  </Form.Label>
+                  <Form.Control
+                    as="textarea"
+                    rows={3}
+                    value={injuriesOrConditions}
+                    onChange={(e) => setInjuriesOrConditions(e.target.value)}
+                    placeholder="Write here..."
+                  />
+                </Form.Group>
+                <Button type="submit" variant="danger" disabled={loading}>
+                  {loading ? "Submitting..." : "Submit"}
+                </Button>
+              </Form>
+            </Card.Body>
+          </Card>
+        )}
+        {isFirstTimeUser === false && (
+          <Card className="my-3" style={styles.card} border="danger">
+            <Card.Body>
+              <Form onSubmit={handleSubmit} style={styles.form}>
+                {/* Customize the returning user form here */}
+                <Form.Group className="mb-3">
+                  <Form.Label htmlFor="progress" style={styles.label}>
+                    How would you rate your progress so far?
+                  </Form.Label>
+                  <Form.Control
+                    as="textarea"
+                    rows={3}
+                    placeholder="Share your experience so far"
+                    value={injuriesOrConditions}
+                    onChange={(e) => setInjuriesOrConditions(e.target.value)}
+                  />
+                </Form.Group>
+                <Form.Group className="mb-3">
+                  <Form.Label htmlFor="fitnessLevel" style={styles.label}>
+                    Fitness Level
+                  </Form.Label>
+                  <Form.Control
+                    as="select"
+                    value={fitnessLevel}
+                    onChange={(e) => setFitnessLevel(e.target.value)}
+                  >
+                    <option value="">Select Your Stage</option>
+                    <option value="Stage 1 (Beginner)">
+                      Stage 1 (Beginner)
+                    </option>
+                    <option value="Stage 2 (Intermediate)">
+                      Stage 2 (Intermediate)
+                    </option>
+                    <option value="Stage 3 (Advanced)">
+                      Stage 3 (Advanced)
+                    </option>
+                  </Form.Control>
+                </Form.Group>
+                <Form.Group className="mb-3">
+                  <Form.Label htmlFor="newGoals" style={styles.label}>
+                    Have your goals changed?
+                  </Form.Label>
+                  <Form.Control
+                    as="select"
+                    value={goals}
+                    onChange={(e) => setGoals(e.target.value)}
+                  >
+                    <option value="">Select Fitness Goal</option>
+                    <option value="fat-loss">Fat Loss</option>
+                    <option value="build-muscle">Build Muscle</option>
+                    <option value="build-strength">Build Strength</option>
+                    <option value="all-of-the-above">All of the Above</option>
+                  </Form.Control>
+                </Form.Group>
+                {/* You can add more fields as needed */}
+                <Button type="submit" variant="danger" disabled={loading}>
+                  {loading ? "Submitting..." : "Submit"}
+                </Button>
+              </Form>
+            </Card.Body>
+          </Card>
+        )}
       </Container>
     </section>
   );
